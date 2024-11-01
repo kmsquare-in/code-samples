fc0c9f5c2dae09b10a164915faad62e17f5a8c74
Kaja Mohideen <reachme@kajasweb.com>
2024-11-01 18:37:00 +0530

    Saving work

---
+++ java-nashorn-graal-comparison/runtime-comparison/src/main/resources/find_prime_numbers_till_N.mjs
@@ -0,0 +1,29 @@
+function NumberHolder(n) {
+    return {
+        value: n,
+        marked: false
+    };
+}
+
+filterNumbersByMultiplesOfP (numbers, p) {
+    return numbers.filter (function (number) {
+        return number 
+    });
+}
+
+function find_last_prime_within_n(n) {
+    if (n < 2) {
+        throw Error ("n cannot be less than 2");
+    }
+    let numbers = [];
+    for (let i = 1; i < n; i++) {
+        numbers = new NumberHolder(n);
+    }
+    let p = 2;
+    
+    for (;;) {
+        
+    }
+}
+
+find_last_prime_within_n (input.getValueOfN());
